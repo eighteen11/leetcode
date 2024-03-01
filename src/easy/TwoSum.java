06dad737f4bb7af8c71b698ad356fe7e348c01f2
Apple <>
2024-03-01 23:06:49 +0000

    Handled corner cases

--- src/easy/TwoSum.java
+++ src/easy/TwoSum.java
@@ -4,18 +4,19 @@ import java.util.Arrays;
 
 public class TwoSum {
     public int[] twoSum(int[] nums, int target) {
-        int i = 0;
-        while (i < nums.length - 1) {
-            if (nums[i] + nums[i + 1] == target) {
-                return new int[]{i, i + 1};
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i; j <nums.length - 1; j++) {
+                if(nums[i] + nums[j + 1] == target){
+                    return new int[]{i, j + 1};
+                }
             }
-            i++;
         }
+
         return new int[]{};
     }
     public static void main(String[] args) {
         TwoSum twoSum = new TwoSum();
-        System.out.println(Arrays.toString(twoSum.twoSum(new int[]{1, 2, 3, 4}, 7)));
+        System.out.println(Arrays.toString(twoSum.twoSum(new int[]{2,5,5,11}, 10)));
     }
 }
 
